734e7054fa4590c24f9122b41954a9e5eb6c114a
Miguel Angel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2021-04-11 21:04:12 -0500

    Juego de ahorcado terminado

--- proyecto_2/juego_del_ahorcado.py
+++ proyecto_2/juego_del_ahorcado.py
@@ -55,7 +55,7 @@ HANGMANPICS = ['''
 
 
 def clearTerminal():
-  if platform.system() == "Linux" or platform.system() == "Darwin":
+  if platform.system() == 'Linux' or platform.system() == 'Darwin':
     os.system('clear')
   else:
     os.system('cls')
@@ -65,37 +65,49 @@ def play_game():
   with open('./archivos/data.txt' , 'r' , encoding='utf-8') as f:
     words = [word for word in f]
 
-    random_index = random.randint(1 , len(words))
+    random_number = random.randint(1 , len(words))
 
     blank_word = []
 
-    for _ in range(len(words[random_index]) -1 ):
-      blank_word.append(" _ ")
+    for _ in range(len(words[random_number]) -1 ):
+      blank_word.append(' _ ')
 
     listToString = ''.join(map(str , blank_word))
 
     menu = f"""
     ¡Bienvenido al juego del ahorcado!
-    {listToString} = {words[random_index]}
+    {listToString}
     Adivina la palabra misteriosa
+    ANTES DE JUGAR: Toma en cuenta que las letras con tildes serán diferentes a las letras sin tildes.
     """
     print(menu)
 
+    flag_win = False
+
     try:
-      while True:
-        newLetter = input("Inserta una letra: ")
+      tries = -1
+      while not flag_win:
+        newLetter = input('Inserta una letra: ')
         if type(newLetter) != str:
-          raise ValueError("Solo puedes insertar letras")
-        if newLetter in words[random_index]:
+          raise ValueError('Solo puedes insertar letras')
+        if newLetter in words[random_number]:
+          tries += 1
           print('Correcto')
-          word_to_list = list(words[random_index])
-          for i in word_to_list:
-            if i == newLetter:
-              blank_word.pop(word_to_list.index(i))
-              blank_word.insert(word_to_list.index(i) , newLetter)
+          word_to_list = list(words[random_number])
+          for count , letter in enumerate(word_to_list):
+            if letter == newLetter:
+              blank_word[count] = newLetter
           print(blank_word)
+          if ' _ ' not in blank_word:
+            print(f'¡Has ganado! Te ha tomado: {tries+1} intentos')
+            flag_win = True
         else:
           print('Incorrecto')
+          tries+=1
+          print(HANGMANPICS[tries])
+          if tries == 6:
+            print(f'¡Has fallado! :( La palabra era: {words[random_number]}')
+            break
     except ValueError as ve:
       print(ve)
 
